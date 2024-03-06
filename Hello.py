9e03d4e12ebcafc64261a7142accf819b8f8f9f1
Thejani <thejani@Thejanis-MacBook-Pro.local>
2024-03-06 07:21:49 +0530

    add pysqlite3

--- Hello.py
+++ Hello.py
@@ -13,6 +13,10 @@ import os
 import openai
 import sqlite3
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 os.environ['OPENAI_API_KEY'] = st.secrets["OPENAI_API_KEY"]
 
